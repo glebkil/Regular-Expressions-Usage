7a94fcf9eae9a4b19cf6e3095a33e0582f8d96ed
Gleb Kilichenko <glebk98@gmail.com>
2018-03-05 18:34:50 +0200

    Adding solution for simple date validation

---
+++ YearValidation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+/*Написать приложение, проверяющее с помощью регулярного
+ * выражения корректность ввода даты в формате Число-Месяц-Год (например, 01-04-2015)*/
+
+namespace YearValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex reg = new Regex(@"^(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[012])-((19|20)\d\d)$");
+            while (true)
+            {
+                Console.WriteLine("Input any date in format of dd-mm-yyyy");
+                if (reg.IsMatch(Console.ReadLine()))
+                {
+                    Console.WriteLine("Input is correct");
+                }
+                else
+                {
+                    Console.WriteLine("Input is incorrect");
+                }
+            }
+        }
+    }
+}
