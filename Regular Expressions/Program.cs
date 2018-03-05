e06fcd578486d095bf3d309156585605a7040138
Gleb Kilichenko <glebk98@gmail.com>
2018-03-05 17:52:28 +0200

    Simple name checker added

--- Regular Expressions/Program.cs
+++ Regular Expressions/Program.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+
+/*1. Написать приложение, проверяющее
+ * с помощью регулярного выражения корректность ввода фамилии и инициалов 
+ * пользователя в следующем виде: Иванов И.И. либо Иванов ИИ*/
 
 namespace Regular_Expressions
 {
@@ -9,6 +14,19 @@ namespace Regular_Expressions
     {
         static void Main(string[] args)
         {
+            Regex reg = new Regex("^[А-Я][а-я]+ ([А-Я].[А-Я].|[А-Я][А-Я])$");
+            while (true)
+            {
+                Console.WriteLine("Input you name and initials: ");
+                if (reg.IsMatch(Console.ReadLine()))
+                {
+                    Console.WriteLine("Input is correct");
+                }
+                else
+                {
+                    Console.WriteLine("Input is incorrect");
+                }
+            }
         }
     }
 }
